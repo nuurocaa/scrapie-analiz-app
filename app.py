f1e8a7e14fd3bf2a81216d93aab2d725dceb0cea
Nur Kırat <nurkocaa97@gmail.com>
2025-11-14 11:37:37 +0300

    fix: App baslatma hatasi icin app.py dosyasi geri yuklendi

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.title("Scrapie Genetik Risk Analizi Uygulaması")
+st.markdown("---")
+st.markdown("""
+**Hoş Geldiniz!**
+Lütfen sol menüden ilgili sayfaya giderek **Scrapie Risk Skoru Hesaplama**, **Scrapie Hastalığı Hakkında Genel Bilgiler** veya **Örnek Çalışmalar"" sayfalarına ulaşınız.
+""")
