29f81ba354bef060ce85a1eb5410c0809f98cdbc
Nur Kırat <nurkocaa97@gmail.com>
2025-11-14 11:33:52 +0300

    fix: Streamlit'in baslatmasi icin ana app.py dosyasi olusturuldu

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(layout="wide")
+st.title("🐑 Scrapie Genetik Risk Analizi Uygulaması")
+st.markdown("---")
+st.markdown("""
+**Hoş Geldiniz!**
+Lütfen sol menüden ilgili sayfaya giderek **Scrapie Risk Skoru Hesaplama** veya **Scrapie Hastalığı Hakkında Bilgi** sayfalarına ulaşınız.
+""")
