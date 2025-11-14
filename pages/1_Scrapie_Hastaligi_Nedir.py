3867299950df0d88a5cf9d2f3be00d92b4138329
Nur Kırat <nurkocaa97@gmail.com>
2025-11-14 17:19:19 +0300

    fix: Sayfa 1'deki sözdizimi hatasi duzeltildi ve icerik tamamlandi.

--- pages/1_Scrapie_Hastaligi_Nedir.py
+++ pages/1_Scrapie_Hastaligi_Nedir.py
@@ -6,7 +6,9 @@ st.title("Scrapie ve Genetik Analiz Hakkında")
 st.markdown("""
 ### 1. Scrapie Hastalığına Giriş
 
-Scrapie, **keçilerde** görülen, fatal (ölümcül) ve dejeneratif bir prion hastalığıdır. Hastalık, beynin normal hücre proteinlerinin (PrPC) yanlış katlanması sonucu anormal, bulaşıcı prion proteinlerine (PrPSc) dönüşmesiyle karakterizedir. Bu prionlar sinir dokularında birikerek nörolojik semptomlara yol açar.
+Scrapie, **koyun ve keçilerde** görülen, fatal (ölümcül) ve dejeneratif bir prion hastalığıdır. Hastalık, beynin normal hücre proteinlerinin (PrPC) yanlış katlanması sonucu anormal, bulaşıcı prion proteinlerine (PrPSc) dönüşmesiyle karakterizedir. Bu prionlar sinir dokularında birikerek nörolojik semptomlara yol açar.
+
+Bu hastalık 250 yıldan fazla bir zamandır bilinen (ilk defa 1732 yılında İngiltere’de farkına varılmıştır) TSE grubu bir hastalıktır (Poser 2002). Hastalığın sığırlarda ortaya çıkan formu, halk arasında deli dana hastalığı olarak bilinen BSE (Bovine spongiform encephalopathy) ise ilk defa 1986 yılında İngiltere’de görülmüştür. BSE’nin ortaya çıkışıyla ilgili en önemli hipotez, BSE’nin koyunlarda görülen scrapie hastalığından köken almış olabileceğidir. Koyun ve keçi artıkları ile kadavralarının hayvan yemi üretiminde kullanılması, BSE’nin koyunlardan sığırlara bulaştığı görüşünü destekleyen önemli bir olgudur (Goldmann et al. 1994, Hunter 1997, Arnold et al. 2002, Sipos et al. 2002, Philippe et al. 2005, Molina et al. 2006, Hunter 2007, Martin et al. 2007, Ün et al. 2008). 
 
 Hastalık, sinir sistemini etkileyerek hayvanlarda kaşıntı (scrapie ismini buradan alır), titreme, koordinasyon bozukluğu ve sonunda ölüme neden olur.
 
